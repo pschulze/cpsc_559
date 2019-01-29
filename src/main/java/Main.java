3e1fc2afbedbc69bdcdc84258247ce586de61885
Peter Schulze <petschul@cisco.com>
2019-01-29 14:01:10 -0700

    use Gson for JSON parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dog.Dog;
 import dog.DogController;
 import io.javalin.Javalin;
+import io.javalin.json.JavalinJson;
+import io.javalin.validation.JavalinValidation;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Main {
 
   public static void main(String[] args) {
+    Gson gson = new GsonBuilder().create();
+    JavalinJson.setFromJsonMapper(gson::fromJson);
+    JavalinJson.setToJsonMapper(gson::toJson);
+
     Javalin app = Javalin.create().start(7000);
     app.routes(() -> {
       path("dogs", () -> {
