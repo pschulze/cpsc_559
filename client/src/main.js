311aa6925109787745ac67e3012fd4e78a25a2d4
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2019-02-04 14:08:29 -0700

    First draft of fetching and displaying from the api

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+
+Vue.prototype.$api = axios.create({ baseURL: "http://localhost:7000/" });
 
 Vue.config.productionTip = false;
 
---
+++ client/src/views/Dogs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dogs">
+    <DogsList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DogsList from "@/components/DogsList.vue";
+
+export default {
+  name: "dogs",
+  components: {
+    DogsList
+  }
+};
+</script>
