ab5a9e6f1fd69ff381c6a3b8e867a35e4351cc68
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2019-03-11 20:39:32 -0600

    Update API url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:7000/"
-  //baseURL: "http://ec2-3-95-28-219.compute-1.amazonaws.com:7000/"
+  baseURL: "http://ec2-3-88-0-236.compute-1.amazonaws.com:975/"
 });
