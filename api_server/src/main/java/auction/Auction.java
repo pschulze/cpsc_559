b809af275d389be7d6f8366ac60dc3c22947181f
sunahkim15 <sunah.kim@ucalgary.ca>
2019-03-12 15:23:53 -0600

    just added getter and setters

--- api_server/src/main/java/auction/Auction.java
+++ api_server/src/main/java/auction/Auction.java
@@ -4,12 +4,38 @@ import dog.Dog;
 
 public class Auction {
 
-    private String host;
-    private Dog dog;
+    private String hostId;
+    private String dogId;
     private int duration;
     private int highestBid;
-    private String highestBidder;
+    private String highestBidderId;
 
+    public String getHostId() { return hostId; }
 
+    public String getDogId() { return dogId; }
+
+    public int getDuration() { return duration; }
+
+    public int getHighestBid() { return highestBid; }
+
+    public String getHighestBidderId() { return highestBidderId; }
+
+    public void setHostId(String hostId) { this.hostId = hostId}
+
+    public void setDogId(String dogId) { this.dogId = dogId; }
+
+    public void setDuration(int duration) { this.duration = duration; }
+
+    public void setHighestBid(int highestBid) { this.highestBid = highestBid; }
+
+    public void setHighestBidderId(String highestBidderId) { this.highestBidderId = highestBidderId; }
+
+    Auction() {}
+
+    Auction(String hostId, String dogId, int duration) {
+        this.hostId = hostId;
+        this.dogId = dogId;
+        this.duration = duration;
+    }
 
 }
