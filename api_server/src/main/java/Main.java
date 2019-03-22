e7805bc29374055129ec50b874f999a9a996c9ad
Peter Schulze <peterkschulze@gmail.com>
2019-03-22 14:51:23 -0600

    Add more routes

--- api_server/src/main/java/Main.java
+++ api_server/src/main/java/Main.java
@@ -32,20 +32,28 @@ public class Main {
     Javalin app = Javalin.create().enableCorsForAllOrigins().start(7000);
 
     app.exception(NumberFormatException.class, ((e, ctx) -> {
-      Map<String, String> message = new HashMap<>();
-      message.put("status", "404");
+      Map<String, Object> message = new HashMap<>();
+      message.put("status", 404);
       message.put("details", e.getMessage());
       ctx.status(404).json(message);
     }));
 
     app.exception(JsonSyntaxException.class, ((e, ctx) -> {
-      Map<String, String> message = new HashMap<>();
-      message.put("status", "400");
+      Map<String, Object> message = new HashMap<>();
+      message.put("status", 400);
       message.put("details", e.getMessage());
       ctx.status(400).json(message);
     }));
 
     app.routes(() -> {
+      path("users", () -> {
+        get(UserController.getAll);
+        post(UserController.create);
+        path(":id", () -> {
+          get(UserController.get);
+//          delete(UserController.delete);
+        });
+      });
       path("dogs", () -> {
         get(DogController.getAll);
         post(DogController.create);
@@ -54,26 +62,14 @@ public class Main {
           patch(DogController.update);
         });
       });
-      path("users", () -> {
-        get(UserController.getAll);
-        post(UserController.create);
-        path(":username", () -> {
-          get(UserController.get);
-//          delete(UserController.delete);
-        });
-      });
       path("auctions", () -> {
-
-      });
-      path("account", () -> {
-        path(":myDogs", () -> {
-
-        });
-        path(":myAuctions", () -> {
-
+        get(AuctionController.getAll);
+        post(AuctionController.create);
+        path(":id", () -> {
+          get(AuctionController.get);
+          post(AuctionController.placeBid);
         });
       });
-
     });
   }
 }
