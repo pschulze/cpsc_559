485df3bf50c0c4d72a6db83df86d80be57c03b41
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2019-03-23 19:44:15 -0600

    Strip any trailing slashes from basepath, will manually add if needed

--- client/src/api/baseroute.js
+++ client/src/api/baseroute.js
@@ -2,7 +2,7 @@ import axios from "./axios";
 
 export default class BaseRoute {
   constructor(basepath) {
-    this.basepath = basepath;
+    this.basepath = basepath.replace(/\/$/, "");
   }
 
   getAll() {
