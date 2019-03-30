dbb703f34e98fafac65f1a1670930ff0368c0bad
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2019-03-29 21:29:22 -0600

    Retrieving user bids

--- client/src/api/account.js
+++ client/src/api/account.js
@@ -1,12 +1,20 @@
 import axios from "./axiosconfig";
 
 export class AccountRoute {
+  constructor() {
+    this.basepath = "account";
+  }
+
   login(username) {
-    return axios.get("account/login/user?username=" + username);
+    return axios.get(this.basepath + "/login/user?username=" + username);
   }
 
   create(username) {
-    return axios.post("account", { username });
+    return axios.post(this.basepath, { username });
+  }
+
+  getBids(id) {
+    return axios.get(this.basepath + "/" + id + "/bids");
   }
 }
 
