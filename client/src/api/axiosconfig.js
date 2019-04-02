ff46bfc0bf00ad478f2ca049e4bcce6e4f91d963
unknown <selena.yu@ucalgary.ca>
2019-04-01 23:37:21 -0600

    Partial Search Terms

--- client/src/api/axiosconfig.js
+++ client/src/api/axiosconfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { responseError } from "./errorHandler";
 
 const instance = axios.create({
-  baseURL: "http://localhost:975/"
+  baseURL: "http://ec2-3-88-0-236.compute-1.amazonaws.com:975/"
 });
 
 // Transform responses before sending onto rest of app
