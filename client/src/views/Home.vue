d93ea45c68dcb1dceff4b2ba2b6f92b28b1a5833
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2019-04-01 18:29:34 -0600

    Add tagline

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,20 +5,7 @@
         <img alt="Vue logo" src="../assets/256px-Black_Paw.svg.png" />
         <h1 class="display-4">The Puppy Project</h1>
         <p class="lead">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium
-          facilisis neque, non molestie ipsum convallis sit amet. Vestibulum sed
-          felis ut purus auctor malesuada. Curabitur facilisis turpis ac lorem
-          scelerisque imperdiet. Cras ligula neque, pulvinar ut rhoncus vitae,
-          bibendum vitae ipsum. Ut ac maximus quam. Curabitur a posuere enim.
-          Duis malesuada est in urna hendrerit, at euismod eros sollicitudin.
-          Nullam a augue id ex tincidunt convallis. Aliquam nec congue eros,
-          vitae elementum turpis. In hac habitasse platea dictumst. Ut non quam
-          aliquam, maximus tortor vel, eleifend nisl. Fusce vulputate mauris mi,
-          et suscipit lectus gravida eu. Praesent sodales, mauris quis dignissim
-          rutrum, nisi ipsum scelerisque ipsum, vel tristique ex risus eget
-          erat. Curabitur finibus velit purus, vitae sodales nibh imperdiet sit
-          amet. Morbi porta risus fringilla, iaculis arcu ornare, feugiat
-          libero.
+          An online auction site for dogs.
         </p>
         <p>
           <router-link class="btn btn-primary my-2 my-sm-0" to="/auctions"
